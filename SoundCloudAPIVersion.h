1269db1915ac3040d65e2a0e1b4be6bf310de7bd
Artem Meleshko <>
2019-05-05 20:51:56 +0300

    minor

---
+++ SoundCloudAPIVersion.h
@@ -0,0 +1,13 @@
+//
+//  SoundCloudAPIVersion.h
+//  Everapp
+//
+//  Created by Artem on 4/21/19.
+//  Copyright © 2019 Everappz. All rights reserved.
+//
+
+#ifndef SoundCloudAPIVersion_h
+#define SoundCloudAPIVersion_h
+
+
+#endif /* SoundCloudAPIVersion_h */
